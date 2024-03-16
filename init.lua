67e5c6bf1a45afdd7c804acf4a30e588abdc79fd
Khawaja Shaheryar <behzadshaheryar@gmail.com>
2024-03-16 17:44:05 +0500

    Added Transparent variable

--- init.lua
+++ init.lua
@@ -2,3 +2,4 @@ require('plugins')
 require('configs')
 require('keybindings')
 require('plugin_config')
+
--- lua/plugin_config/gruvbox.lua
+++ lua/plugin_config/gruvbox.lua
@@ -1,2 +1,8 @@
+--require("gruvbox").setup({
+--    transparent_mode = true,
+--})
+
 vim.o.termguicolors = true
 vim.cmd [[ colorscheme gruvbox ]]
+
+vim.g.gruvbox_italic = 1
