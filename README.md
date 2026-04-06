557e7e60e404db8d9721b96f6c478a7e18456077
RNGesus <behzadshaheryar@gmail.com>
2026-04-06 15:52:14 +0500

    Fixed minor bugs

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nvim-config
 My personal NeoVim configuration using Lua. I am using Packer as a package manager.
+
+# NOTES
+- NerdFont
+- tree-sitter (RUST)
+
+For Windows you will need to configure alot of things using scoop and winget.
+- diffutils for undo extension
+- setting gcc as default compiler in terminal for tree-sitter
+- setting git path in terminal
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -53,6 +53,16 @@ require("lazy").setup({
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function() require("plugin_config.telescope") end,
     },
+    {
+    "amitds1997/remote-nvim.nvim",
+        version = "*",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-telescope/telescope.nvim",
+        },
+        config = true,
+    },
 
     -- LSP + Completion
     {
